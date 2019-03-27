edd25ef8ba4d2181fcc6540af0e00e75f8834b2e
Stéphane Goudeau <stephgou@microsoft.com>
2019-03-27 19:08:58 +0100

    First commit

---
+++ Deploy-ARM-SQLAW.ps1
@@ -0,0 +1,49 @@
+﻿Param(  
+    # Name of the subscription to use for azure cmdlets
+    [string] $subscriptionName = "StephgouInTheCloud",
+    [string] $subscriptionId = "0459dbd5-b73e-4a5b-b052-250dc51ac622",
+    #Paramètres du Azure Ressource Group
+    $prefix = "CEGID-SQLAW",
+    $resourceGroupName = "SG-RG-" + $prefix.ToUpper(),
+    $resourceLocation = "West Europe",
+	$resourceGroupDeploymentName = "$prefix-Deployed",
+    $templateFile = "azuredeploy.json",
+    $templateParameterFile = "azuredeploy.parameters.json",
+    $templateFolder = ".",
+    $tagName = "$prefix_RG",
+    $tagValue = "$prefix"
+    )
+
+#region init
+Set-PSDebug -Strict
+
+cls
+$d = get-date
+Write-Host "Starting Deployment $d"
+
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Write-Host "scriptFolder" $scriptFolder
+
+set-location $scriptFolder
+#endregion init
+
+#Login-AzureRmAccount -SubscriptionId $subscriptionId
+#Get-AzureRmResourceGroup -Name $resourceGroupName -ev notPresent -ea 0
+
+# Resource group create
+New-AzureRmResourceGroup `
+	-Name $resourceGroupName `
+	-Location $resourceLocation `
+    -Tag @{Name=$tagName;Value=$tagValue} `
+    -Verbose
+
+# Resource group deploy
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceGroupDeploymentName `
+	-ResourceGroupName $resourceGroupName `
+	-TemplateFile "$scriptFolder\$templatefolder\$templateFile" `
+	-TemplateParameterFile "$scriptFolder\$templatefolder\$templateParameterFile" `
+    -Debug -Verbose -DeploymentDebugLogLevel All
+
+$d = get-date
+Write-Host "Stopping Deployment $d"
---
+++ readme.md
@@ -0,0 +1,20 @@
+## SQL Always On Template ##
+
+This repository is dedicated to the implementation of an ARM Template based on SQL Virtual Machine resource provider.
+It intends to follow the process described in the following article and automate what is still a manual operation.
+It is supposed to be run on an existing infrastructure running 2 SQL Servers.
+
+### Automate Always On availability group deployments with SQL Virtual Machine resource provider  ###
+
+http://blog.mashfords.com/2018/12/13/automate-always-on-availability-group-deployments-with-sql-virtual-machine-resource-provider/
+
+### Create WS Failover Cluster and join existing SQL Server Virtual Machines for setting up an Always On availability group ###
+
+https://github.com/Azure/azure-quickstart-templates/tree/master/101-sql-vm-ag-setuphttps://github.com/Azure/azure-quickstart-templates/tree/master/101-sql-vm-ag-setup
+
+### Setting up AlwaysOn Availability Groups ###
+
+https://www.ericcsinger.com/powershell-scripting-installing-sql-setting-up-alwayson-availability-groups/
+
+### Configure ILB and create listener for an existing Always On availability group on SQL Server VMs in Azure ###
+https://github.com/Azure/azure-quickstart-templates/tree/master/101-sql-vm-aglistener-setup
